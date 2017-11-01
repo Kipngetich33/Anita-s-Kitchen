21170408e0b42eb083ee6798114feedc5155fb3c
Vincent Ngeno <khalifngeno@gmail.com>
2017-11-01 10:03:01 +0300

    modify the jquery function to notify the user that their email has been added to a list of subscribers

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,10 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.height = "0%";
 }
+jQuery(document).ready(function() {  
+  jQuery("#subscribe-btn").click(function() {
+
+  	var email= $("#email").val();
+    alert(email+" "+"has been successfully added to our list of subscribers.Thank you!");
+  });
+});
